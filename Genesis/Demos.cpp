10d62ec998156c16a27e884cf6a3c8651962ea02
Andrew Pham <andrew.tucker.p@gmail.com>
2015-10-08 00:15:08 -0700

    New demo on Bezier curves

--- Genesis/Demos.cpp
+++ Genesis/Demos.cpp
@@ -34,6 +34,190 @@ bool enable_displacement = false;
 bool wireframe = false;
 bool enable_fog = false;
 
+// Additional Cubic Bezier Controls
+bool show_points = false;
+bool show_cage = false;
+
+void render_superbible_cubicbezier(GLFWwindow* window)
+{
+	// Setup and compile our shaders
+	Shader tess_shader("Shaders/cubicbezier.vs", "Shaders/cubicbezier.frag", "Shaders/cubicbezier.tcs", "Shaders/cubicbezier.tes");
+	Shader draw_cp_shader("Shaders/draw-control-points.vs", "Shaders/draw-control-points.frag");
+	
+	GLuint patch_vao;
+	GLuint patch_buffer;
+	GLuint cage_indices;
+	glm::vec3 patch_data[16];
+
+	struct
+	{
+		struct
+		{
+			int mv_matrix;
+			int proj_matrix;
+			int mvp;
+		} patch;
+		struct
+		{
+			int draw_color;
+			int mvp;
+		} control_point;
+	} uniforms;
+
+	tess_shader.Use();
+
+	uniforms.patch.mv_matrix = glGetUniformLocation(tess_shader.Program, "mv_matrix");
+	uniforms.patch.proj_matrix = glGetUniformLocation(tess_shader.Program, "proj_matrix");
+	uniforms.patch.mvp = glGetUniformLocation(tess_shader.Program, "mvp");
+
+	draw_cp_shader.Use();
+
+	uniforms.control_point.draw_color = glGetUniformLocation(draw_cp_shader.Program, "draw_color");
+	uniforms.control_point.mvp = glGetUniformLocation(draw_cp_shader.Program, "mvp");
+
+	glGenVertexArrays(1, &patch_vao);
+	glBindVertexArray(patch_vao);
+
+	glGenBuffers(1, &patch_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, patch_buffer);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(patch_data), NULL, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	static const GLushort indices[] =
+	{
+		0, 1, 1, 2, 2, 3,
+		4, 5, 5, 6, 6, 7,
+		8, 9, 9, 10, 10, 11,
+		12, 13, 13, 14, 14, 15,
+
+		0, 4, 4, 8, 8, 12,
+		1, 5, 5, 9, 9, 13,
+		2, 6, 6, 10, 10, 14,
+		3, 7, 7, 11, 11, 15
+	};
+
+	glGenBuffers(1, &cage_indices);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cage_indices);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check and call events
+		glfwPollEvents();
+		Do_Movement();
+
+		// Clear buffers
+		static const GLfloat gray[] = { 0.1f, 0.1f, 0.1f, 0.0f };
+		static const GLfloat one = 1.0f;
+
+		int i;
+		static double last_time = 0.0;
+		static double total_time = 0.0;
+
+		GLfloat currentTime = glfwGetTime();
+		if (!paused)
+			total_time += (currentTime - last_time);
+		last_time = currentTime;
+
+		float t = (float)total_time;
+
+		static const float patch_initializer[] =
+		{
+			-1.0f,  -1.0f,  0.0f,
+			-0.33f, -1.0f,  0.0f,
+			0.33f, -1.0f,  0.0f,
+			1.0f,  -1.0f,  0.0f,
+
+			-1.0f,  -0.33f, 0.0f,
+			-0.33f, -0.33f, 0.0f,
+			0.33f, -0.33f, 0.0f,
+			1.0f,  -0.33f, 0.0f,
+
+			-1.0f,   0.33f, 0.0f,
+			-0.33f,  0.33f, 0.0f,
+			0.33f,  0.33f, 0.0f,
+			1.0f,   0.33f, 0.0f,
+
+			-1.0f,   1.0f,  0.0f,
+			-0.33f,  1.0f,  0.0f,
+			0.33f,  1.0f,  0.0f,
+			1.0f,   1.0f,  0.0f,
+		};
+
+		glViewport(0, 0, screenWidth, screenHeight);
+		glClearBufferfv(GL_COLOR, 0, gray);
+		glClearBufferfv(GL_DEPTH, 0, &one);
+
+		glEnable(GL_DEPTH_TEST);
+
+		glm::vec3 * p = (glm::vec3 *)glMapBufferRange(GL_ARRAY_BUFFER, 0, sizeof(patch_buffer), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+		memcpy(p, patch_initializer, sizeof(patch_initializer));
+
+		for (i = 0; i < 16; i++)
+		{
+			float fi = (float)i / 16.0f;
+			p[i][2] = sinf(t * (0.2f + fi * 0.3f));
+		}
+
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+
+		glBindVertexArray(patch_vao);
+
+		tess_shader.Use();
+
+		glm::mat4 proj_matrix = glm::perspective(50.0f,
+			(float)screenWidth / (float)screenHeight,
+			1.0f, 1000.0f);
+
+		glm::mat4 mv_matrix;
+		mv_matrix = glm::translate(mv_matrix, glm::vec3(0.0f, 0.0f, -14.0f));
+		mv_matrix = glm::rotate(mv_matrix, t * 10.0f * PI_F / 180.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+		mv_matrix = glm::rotate(mv_matrix, t * 17.0f * PI_F / 180.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+
+		glUniformMatrix4fv(uniforms.patch.mv_matrix, 1, GL_FALSE, glm::value_ptr(mv_matrix));
+		glUniformMatrix4fv(uniforms.patch.proj_matrix, 1, GL_FALSE, glm::value_ptr(proj_matrix));
+		glUniformMatrix4fv(uniforms.patch.mvp, 1, GL_FALSE, glm::value_ptr(proj_matrix * mv_matrix));
+
+		if (wireframe)
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+		else
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
+
+		glPatchParameteri(GL_PATCH_VERTICES, 16);
+		glDrawArrays(GL_PATCHES, 0, 16);
+
+		draw_cp_shader.Use();
+		glUniformMatrix4fv(uniforms.control_point.mvp, 1, GL_FALSE, glm::value_ptr(proj_matrix * mv_matrix));
+
+		if (show_points)
+		{
+			glPointSize(9.0f);
+			glUniform4fv(uniforms.control_point.draw_color, 1, glm::value_ptr(glm::vec4(0.2f, 0.7f, 0.9f, 1.0f)));
+			glDrawArrays(GL_POINTS, 0, 16);
+		}
+
+		if (show_cage)
+		{
+			glUniform4fv(uniforms.control_point.draw_color, 1, glm::value_ptr(glm::vec4(0.7f, 0.9f, 0.2f, 1.0f)));
+			glDrawElements(GL_LINES, 48, GL_UNSIGNED_SHORT, NULL);
+		}
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		// Swap the buffers
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+}
+
 void render_superbible_dispmap(GLFWwindow* window)
 {
 	// Setup and compile our shaders
@@ -1176,6 +1360,10 @@ void Do_Movement()
 		enable_displacement = !enable_displacement;
 	if (keys[GLFW_KEY_H])
 		wireframe = !wireframe;
+	if (keys[GLFW_KEY_C])
+		show_cage = !show_cage;
+	if (keys[GLFW_KEY_X])
+		show_points = !show_points;
 }
 
 #pragma endregion
